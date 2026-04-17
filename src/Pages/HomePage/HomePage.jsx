e6945ba59267c2a074dcc01649c7d534649f51f6
ShifathPro057 <sharifulamin2023@gmail.com>
2026-04-17 20:05:34 +0600

    HomePage part  done

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../../Component/Banner/Banner';
+import Friends from '../../Component/Friends/Friends';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Friends></Friends>
+        </div>
+    );
+};
+
+export default HomePage;
